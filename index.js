31a6d5c5dc83c7d76ce73d3d9f472fd442652161
fundamentos backend <fundamentos.backend.2023@outlook.com>
2025-07-19 19:20:46 -0600

    add: mw

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
 const express = require('express');
 const app = express();
 
+const globalMw = (req, res, next) => {
+    console.log('Hello from globla mw');
+    next();
+}
+
+const globalMw2 = (req, res, next) => {
+    console.log('Hello from second globla mw');
+    next();
+}
+
+const rootMw = (req, res, next) => {
+    console.log('Hello from root mw');
+    next();
+}
+
+const gmw = (req, res, next) => {
+    console.log('Hello from greeting mw');
+    next();
+}
+
+const exclusiveRootMw = (req, res, next) => {
+    console.log('hello from exclusive');
+    next();
+}
+
+app.use(globalMw2)
+app.use(globalMw);
+app.use('/greeting', gmw);
+app.use('/', rootMw);
+
+
 const port = 3003;
 
-app.get('/', (req, res) => {
-    res.send('Hello from server');
-});
+app.get('/',
+    exclusiveRootMw,
+    (req, res) => {
+        res.send('Hello from server');
+    }
+);
 
 app.get('/greeting', (req, res) => {
     res.status(200).json({'message': 'message in json from server'});
+});
+
+app.get('/greeting2', (req, res) => {
+    res.status(200).json({'message': 'second message in json from server'});
 });
 
 app.listen(port, () => {
